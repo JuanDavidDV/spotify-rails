78f1b13d8e6a47da8375aeeb007d993c95420405
JuanDavidDV <juandadazav@gmail.com>
2025-02-16 21:43:24 -0500

    create song table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_02_17_010219) do
+ActiveRecord::Schema[8.0].define(version: 2025_02_17_023429) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -25,4 +25,14 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_17_010219) do
     t.index ["email"], name: "index_artists_on_email", unique: true
     t.index ["reset_password_token"], name: "index_artists_on_reset_password_token", unique: true
   end
+
+  create_table "songs", force: :cascade do |t|
+    t.string "title"
+    t.bigint "artist_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["artist_id"], name: "index_songs_on_artist_id"
+  end
+
+  add_foreign_key "songs", "artists"
 end
