e2682759fce7ca5607af6084608254d42ed94559
JuanDavidDV <juandadazav@gmail.com>
2025-02-16 21:15:10 -0500

    creates files for dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :artists
   root "home#index"
+
+  resource :dashboard, to: "artists#dashboard"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
