5f4eec7e02a2e9452047c614dbb9743e478d2c54
JuanDavidDV <juandadazav@gmail.com>
2025-02-20 21:36:31 -0500

    audio can now be listened

--- app/javascript/controllers/music_controller.js
+++ app/javascript/controllers/music_controller.js
@@ -2,6 +2,17 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="music"
 export default class extends Controller {
+  static targets = ["icon"];
+  static values = { url: String };
+
   connect() {
+    const audio = new Audio();
+    audio.src = this.urlValue;
+  }
+
+  toggle(e) {
+    e.preventDefault(); // Prevents reload
+    this.iconTargets.forEach(target => target.classList.toggle("hidden"));
+    audio.play();
   }
 }
