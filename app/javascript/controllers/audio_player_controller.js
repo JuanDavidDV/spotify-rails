7ef4a6e50bdf10ed799b44de28e518b10e4d4043
JuanDavidDV <juandadazav@gmail.com>
2025-02-24 10:06:57 -0500

    start developing minutes format for audio player

--- app/javascript/controllers/audio_player_controller.js
+++ app/javascript/controllers/audio_player_controller.js
@@ -4,6 +4,24 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["currentTime", "duration", "range"];
   connect() {
-    console.log("The audio playing is:", window.audio)
+    if (!window.audio) return 
+    console.log(window.audio.duration);
+    if (window.audio.duration) {
+      console.log("has duration", window.audio.duration);
+      this.rangeTarget.max = window.audio.duration;
+      this.durationTarget.innerHTML = formatTime(window.audio.duration);
+    }
+    this.rangeTarget.value = 0;
+
+    window.audio.addEventListener("timeupdate", () => {
+      console.log("Time updated event", formatTime(window.audio.duration));
+      this.currentTimeTarget.innerHTML = formatTime(window.audio.duration); 
+    })
   }
+};
+
+const formatTime = (seconds) => {
+  const minutes = Math.floor(seconds / 60);
+  const second = (seconds % 60);
+  return minutes + ":" + (second < 10 ? "0" + second : seconds);
 }
