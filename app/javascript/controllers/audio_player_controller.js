52bc03812f599c443d9173074915cf8ff537bf46
JuanDavidDV <juandadazav@gmail.com>
2025-02-24 08:44:40 -0500

    update audio style and create static variables

--- app/javascript/controllers/audio_player_controller.js
+++ app/javascript/controllers/audio_player_controller.js
@@ -2,6 +2,8 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="audio-player"
 export default class extends Controller {
+  static targets = ["currentTime", "duration", "range"];
   connect() {
+    console.log("The audio playing is:", window.audio)
   }
 }
