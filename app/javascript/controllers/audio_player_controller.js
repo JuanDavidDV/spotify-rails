2bd6d9d3ec9195f2194856560c6d38f3c8a42d5c
JuanDavidDV <juandadazav@gmail.com>
2025-02-25 08:42:02 -0500

    finalized function for audio player time

--- app/javascript/controllers/audio_player_controller.js
+++ app/javascript/controllers/audio_player_controller.js
@@ -5,23 +5,24 @@ export default class extends Controller {
   static targets = ["currentTime", "duration", "range"];
   connect() {
     if (!window.audio) return 
-    console.log(window.audio.duration);
-    if (window.audio.duration) {
-      console.log("has duration", window.audio.duration);
-      this.rangeTarget.max = window.audio.duration;
-      this.durationTarget.innerHTML = formatTime(window.audio.duration);
-    }
     this.rangeTarget.value = 0;
 
-    window.audio.addEventListener("timeupdate", () => {
-      console.log("Time updated event", formatTime(window.audio.duration));
-      this.currentTimeTarget.innerHTML = formatTime(window.audio.duration); 
+    this.timeUpdateEventListener = window.audio.addEventListener("timeupdate", () => {
+      this.rangeTarget.max = window.audio.duration;
+      this.durationTarget.innerHTML = formatTime(window.audio.duration);
+      this.currentTimeTarget.innerHTML = formatTime(window.audio.currentTime); 
     })
+  }
+
+  // When element gets removed from the page
+  disconnect() { 
+    window.audio.removeEventListener("timeupdate", this.timeUpdateEventListener);
   }
 };
 
-const formatTime = (seconds) => {
-  const minutes = Math.floor(seconds / 60);
-  const second = (seconds % 60);
-  return minutes + ":" + (second < 10 ? "0" + second : seconds);
+const formatTime = (currentTime) => {
+  const minutes = "0" + Math.floor(currentTime / 60);
+  const seconds = "0" + Math.floor(currentTime - minutes * 60);
+  const duration = minutes.substr(-2) + ":" + seconds.substr(-2);
+  return duration;
 }
