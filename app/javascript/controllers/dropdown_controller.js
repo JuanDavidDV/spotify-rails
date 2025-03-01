bffd2bfcea8c420717acc5f544bf129cd3b127ab
JuanDavidDV <juandadazav@gmail.com>
2025-03-01 12:23:44 -0500

    style dropdown

--- app/javascript/controllers/dropdown_controller.js
+++ app/javascript/controllers/dropdown_controller.js
@@ -2,6 +2,9 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="dropdown"
 export default class extends Controller {
-  connect() {
+  static targets = ["menu"];
+  toggle(e) {
+    e.preventDefault();
+    this.menuTarget.classList.toggle("hidden");
   }
 }
