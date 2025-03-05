686022c097bcc1f1d18db5c7f2ee3137eeba1e4f
JuanDavidDV <juandadazav@gmail.com>
2025-03-04 22:33:51 -0500

    add functions

--- app/javascript/controllers/stripe_onboarding_controller.js
+++ app/javascript/controllers/stripe_onboarding_controller.js
@@ -1,7 +1,30 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
+import { loadConnectAndInitialize } from '@stripe/connect-js';
+import { post } from "@rails/request.js";
 
 // Connects to data-controller="stripe-onboarding"
 export default class extends Controller {
   connect() {
+    const fetchClientSecret = async () => {
+      // Fetch the AccountSession client secret
+      const response = await post('/account_session');
+      if (!response.ok) {
+        // Handle errors on the client side here
+        const {error} = await response.json();
+        console.error('An error occurred: ', error);
+        document.querySelector('#error').removeAttribute('hidden');
+        return undefined;
+      } else {
+        const {client_secret: clientSecret} = await response.json();
+        document.querySelector('#error').setAttribute('hidden', '');
+        return clientSecret;
+      }
+    };
+    
+    const accountOnboarding = stripeConnectInstance.create('account-onboarding');
+    accountOnboarding.setOnExit(() => {
+      console.log('User exited the onboarding flow');
+    });
+    container.appendChild(accountOnboarding);
   }
 }
