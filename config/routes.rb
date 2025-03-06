d380dc87d04b7657cd4a3bc252c81b89d0531f51
JuanDavidDV <juandadazav@gmail.com>
2025-03-06 08:27:34 -0500

    create webhooks rout

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,10 @@ Rails.application.routes.draw do
 
   namespace :stripe do
     post "/account-session", to: "stripe#account_session", as: :onboarding_session  # URL for stripe_onboarding controller stimulus js
+    post "/webhooks", to: "stripe#webhooks"
   end
 
-    # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
