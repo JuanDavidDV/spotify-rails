5d3e5faa4171c9ab05ee7934e3657fcb30754758
JuanDavidDV <juandadazav@gmail.com>
2025-03-09 19:42:42 -0400

    add style to artist dashboard

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,4 +1,7 @@
 class ArtistsController < ApplicationController
   def dashboard
+    if current_artist.payouts_enabled?
+      @balance ||= Stripe::Balance.retrieve(current_artist.stripe_account_id)
+    end
   end
 end
