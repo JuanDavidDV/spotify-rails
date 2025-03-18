e70a00807c5416c6eddf6aa95b3fd3adfe6840fc
JuanDavidDV <juandadazav@gmail.com>
2025-03-18 08:58:10 -0400

    add test data to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,14 @@
 artists = [
   {
-    stage_name: "Silvestre Dangon"
+    stage_name: "Silvestre Dangon",
+    email: "artist1@email.com",
+    password: "Password12"
   }
 ]
+
+artists.each do |artist|
+  Artist.find_or_create_by(email: artist[:email]) do |a|  # find_or_create_by used to prevent duplicates when re-running seed file
+    a.stage_name = artist[:stage_name],
+    a.password = artist[:password]
+  end
+end
