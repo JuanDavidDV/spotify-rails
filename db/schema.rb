3cd9e3d7941367a93bd2a1e1913b4b1bd6f1304f
JuanDavidDV <juandadazav@gmail.com>
2025-03-23 15:45:30 -0400

    add new column concurrency key

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_03_23_193147) do
+ActiveRecord::Schema[8.0].define(version: 2025_03_23_194457) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -65,6 +65,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_03_23_193147) do
     t.integer "priority", default: 0
     t.string "queue_name"
     t.string "active_job_id"
+    t.string "concurrency_key"
   end
 
   create_table "songs", force: :cascade do |t|
