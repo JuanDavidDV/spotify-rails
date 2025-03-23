2775549f3e3b0dd29832138f7f8580963d84b66f
JuanDavidDV <juandadazav@gmail.com>
2025-03-23 10:53:00 -0400

    delete solid_queue migrations

--- db/migrate/20250323141955_add_priority_to_solid_queue_jobs.rb
+++
@@ -1,5 +0,0 @@
-class AddPriorityToSolidQueueJobs < ActiveRecord::Migration[8.0]
-  def change
-    add_column :solid_queue_jobs, :priority, :integer
-  end
-end
--- db/migrate/20250323144046_add_scheduled_at_to_solid_queue_jobs.rb
+++
@@ -1,5 +0,0 @@
-class AddScheduledAtToSolidQueueJobs < ActiveRecord::Migration[8.0]
-  def change
-    add_column :solid_queue_jobs, :scheduled_at, :datetime
-  end
-end
