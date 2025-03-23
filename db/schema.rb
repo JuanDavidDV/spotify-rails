74bb66f469dc71fca3e9b12f8ef58e12300744e0
JuanDavidDV <juandadazav@gmail.com>
2025-03-22 23:22:52 -0400

    migrate column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_03_23_025423) do
+ActiveRecord::Schema[8.0].define(version: 2025_03_23_031527) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -66,6 +66,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_03_23_025423) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "queue_name"
+    t.string "active_job_id"
   end
 
   create_table "songs", force: :cascade do |t|
