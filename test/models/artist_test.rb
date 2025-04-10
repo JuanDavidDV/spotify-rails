2591432719640ca924a4748d387bcecd0eb18a9b
JuanDavidDV <juandadazav@gmail.com>
2025-04-09 20:30:21 -0400

    develop first test for a song to must have a title

--- test/models/artist_test.rb
+++ test/models/artist_test.rb
@@ -1,7 +1,13 @@
 require "test_helper"
 
 class ArtistTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @artist = artists(:one)
+    @song = songs(:one)
+  end
+
+  test "title should be present" do
+    @song.title = ""
+    assert_not @song.valid?
+  end
 end
