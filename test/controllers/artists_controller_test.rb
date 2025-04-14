e9e3e2047af5d1f7fc8e0e0141cb2474f7bf195d
JuanDavidDV <juandadazav@gmail.com>
2025-04-14 10:38:50 -0400

    finish developing test for artists controller

--- test/controllers/artists_controller_test.rb
+++ test/controllers/artists_controller_test.rb
@@ -28,4 +28,30 @@ class ArtistsControllerTest < ActionDispatch::IntegrationTest
     # Restore the original method to avoid side effects
     Stripe::Balance.define_singleton_method(:retrieve, original_method)
   end
+
+  test "should get dashboard and NOT retrieve balance if payouts are disabled" do
+    def @artist.payouts_enabled?
+      false
+    end
+  
+    # Set up a flag to track if Stripe::Balance.retrieve was called
+    called = false
+  
+    # Save the original method
+    original_method = Stripe::Balance.method(:retrieve)
+  
+    # Override the method to track calls
+    Stripe::Balance.define_singleton_method(:retrieve) do |*args|
+      called = true
+      original_method.call(*args)
+    end
+  
+    get dashboard_path
+    assert_response :success
+    assert_not called, "Expected Stripe::Balance.retrieve to not be called"
+  
+  ensure
+    # Restore the original method after the test
+    Stripe::Balance.define_singleton_method(:retrieve, original_method)
+  end
 end
