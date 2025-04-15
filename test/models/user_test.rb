8243878249ed86f586b579f1295c6069d1210e75
JuanDavidDV <juandadazav@gmail.com>
2025-04-15 14:37:09 -0400

    start developing user tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,17 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user = users(:one)
+  end
+
+  test "should be valid with fixtures data" do
+    assert @user.valid?
+  end
+
+  test "should be invalid without an email" do
+    @user.email = ""
+    assert_not @user.valid?
+    assert_includes @user.errors[:email], "can't be blank"
+  end
 end
