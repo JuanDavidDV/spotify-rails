c784626d49464e382c7017744fd60292f3c12bbf
JuanDavidDV <juandadazav@gmail.com>
2025-04-15 08:31:07 -0400

    add new test for an artist to have many songs

--- test/models/artist_test.rb
+++ test/models/artist_test.rb
@@ -20,4 +20,8 @@ class ArtistTest < ActiveSupport::TestCase
     assert_not artist.valid?
     assert_includes artist.errors[:password], "can't be blank"
   end
+
+  test "should have many songs" do
+    assert_equal :has_many, Artist.reflect_on_association(:songs).macro # macro returns the type of association defined, in this case "has_many"
+  end
 end
