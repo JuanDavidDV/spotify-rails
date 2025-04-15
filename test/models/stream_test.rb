bce7122b38cccd09f78db6a886e3900edda64fcd
JuanDavidDV <juandadazav@gmail.com>
2025-04-15 14:15:37 -0400

    add test for streams to belong to user

--- test/models/stream_test.rb
+++ test/models/stream_test.rb
@@ -9,4 +9,8 @@ class StreamTest < ActiveSupport::TestCase
   test "should belong to a song" do
     assert_equal :belongs_to, Stream.reflect_on_association(:song).macro
   end
+
+  test "should belong to a user" do
+    assert_equal :belongs_to, Stream.reflect_on_association(:song).macro
+  end
 end
