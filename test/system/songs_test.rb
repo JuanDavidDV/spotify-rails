7d8711ea54cf4ff6584ac7087a6634aead0a68a1
JuanDavidDV <juandadazav@gmail.com>
2025-04-21 14:31:29 -0400

    delete test

--- test/system/songs_test.rb
+++ test/system/songs_test.rb
@@ -55,18 +55,6 @@ class SongsTest < ApplicationSystemTestCase
     assert_selector "h1", text: "Showing song"
   end
 
-  test "should update Song" do
-    visit song_url(@song)
-    click_on "Edit this song", match: :first
-
-    fill_in "Artist", with: @song.artist_id
-    fill_in "Title", with: @song.title
-    click_on "Update Song"
-
-    assert_text "Song was successfully updated"
-    click_on "Back"
-  end
-
   test "should destroy Song" do
     visit song_url(@song)
     click_on "Destroy this song", match: :first
