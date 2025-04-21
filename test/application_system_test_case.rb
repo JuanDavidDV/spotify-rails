e59b7ec484411f5535d6071220e20364fbaaf225
JuanDavidDV <juandadazav@gmail.com>
2025-04-21 11:49:30 -0400

    install locally webdrivers and chrome drivers, create a song in system test now works

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -2,27 +2,50 @@ require "test_helper"
 WebMock.allow_net_connect!
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  WINDOWS_HOST = `ip route | grep default | awk '{print $3}'`.strip
-  CHROMEDRIVER_URL = "http://#{WINDOWS_HOST}:9515/"
-  driven_by :selenium_remote_chrome
+  # Disable webdrivers auto-download since we're using system Chromium
+  Webdrivers.install_dir = "/usr/bin" # Point to where chromedriver is installed
+  Webdrivers::Chromedriver.required_version = `chromedriver --version`.split[1]
 
-  Capybara.register_driver :selenium_remote_chrome do |app|
+  # Register Chromium driver
+  Capybara.register_driver :selenium_chromium do |app|
     options = Selenium::WebDriver::Chrome::Options.new
-    options.add_argument('--start-maximized')
-    options.add_argument('--disable-gpu')
-    options.add_argument('--no-sandbox')
+    options.binary = "/usr/bin/chromium-browser" # Point to Chromium binary
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--window-size=1400,1400")
 
     Capybara::Selenium::Driver.new(
       app,
-      browser: :remote,
-      url: CHROMEDRIVER_URL,  # Use dynamic IP here
-      capabilities: options
+      browser: :chrome,
+      options: options
     )
   end
 
+  # Register headless Chromium driver
+  Capybara.register_driver :selenium_chromium_headless do |app|
+    options = Selenium::WebDriver::Chrome::Options.new
+    options.binary = "/usr/bin/chromium-browser"
+    options.add_argument("--headless=new")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--window-size=1400,1400")
+
+    Capybara::Selenium::Driver.new(
+      app,
+      browser: :chrome,
+      options: options
+    )
+  end
+
+  # Use the Chromium driver by default
+  driven_by :selenium_chromium
+
+  # Configure Capybara for WSL
   Capybara.configure do |config|
-    # Match what's set for URL options in test.rb so we can test mailers that contain links.
-    config.server_host = 'localhost'
-    config.server_port = '3000'
+    config.server_host = '0.0.0.0'
+    config.server_port = 3000
+    config.app_host = "http://localhost:3000" # Or use your WSL IP if needed
   end
 end
--- test/system/songs_test.rb
+++ test/system/songs_test.rb
@@ -11,6 +11,29 @@ class SongsTest < ApplicationSystemTestCase
     assert_selector "[data-music-target='play']"  # This is the play SVG to play a song
   end
 
+
+  test "should create a song" do
+    visit new_artist_session_url
+  
+    fill_in "Email", with: @artist.email
+    fill_in "Password", with: "Password"
+    click_on "Log in"
+  
+    click_on "Post a song"
+  
+    fill_in "Title", with: "Song Test"
+  
+    # Use absolute paths with forward slashes
+    image_path = File.expand_path("test/fixtures/files/images/lion.webp")
+    audio_path = File.expand_path("test/fixtures/files/audios/tiger.mp3")
+  
+    attach_file "Image", image_path
+    attach_file "Audio file", audio_path
+  
+    click_on "Create Song"
+    assert_selector "h1", text: "Showing song"
+  end
+
   test "should edit a song title" do
     visit new_artist_session_url
 
@@ -19,9 +42,11 @@ class SongsTest < ApplicationSystemTestCase
     click_on "Log in"
 
     click_on "View your songs"
-
     click_on "Show this song", match: :first
-    assert_text "Title"
+    click_on "Edit this song"
+    fill_in "Title", with: "New Test"
+    click_on "Update Song"
+    assert_selector "h1", text: "Showing song"
   end
 
   test "should update Song" do
