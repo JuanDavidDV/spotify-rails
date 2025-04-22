4c5dcdc1036867cf68764082313604fa4682e3ac
JuanDavidDV <juandadazav@gmail.com>
2025-04-22 07:38:58 -0400

    create destroy user account and should not create user with missing information test

--- test/system/users_test.rb
+++ test/system/users_test.rb
@@ -23,6 +23,16 @@ class UsersTest < ApplicationSystemTestCase
     assert_text "Welcome! You have signed up successfully."
   end
 
+  test "should not create a user if there is missing information" do
+    visit new_user_registration_url
+    fill_in "Email", with: ""
+    fill_in "Password", with: "Password"
+    fill_in "Password", with: "Password"
+    click_on "Sign up"
+
+    assert_text "Email can't be blank"
+  end
+
   test "should visit the songs page after sign in" do
     log_in_user
     assert_text "Signed in successfully."
@@ -38,5 +48,20 @@ class UsersTest < ApplicationSystemTestCase
     fill_in "Current password", with: "Password"
     click_on "Update"
     assert_text "Your account has been updated successfully."
+  end
+
+  test "Should destroy User" do
+    @user.streams.destroy_all
+
+    log_in_user
+
+    find("a[data-action='dropdown#toggle']").click
+    click_on "Settings"
+
+    accept_confirm do
+      click_on "Cancel my account"
+    end
+
+    assert_text "Bye! Your account has been successfully cancelled. We hope to see you again soon."
   end
 end
